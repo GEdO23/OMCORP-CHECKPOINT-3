413fe7eadb9f387ae93f71fc9c54574bba2520cf
YagoFarah <RM551616@fiap.com.br>
2023-05-22 16:32:10 -0300

    Adicionado JS

--- js/cp3.js
+++ js/cp3.js
@@ -6,3 +6,44 @@ darkModeToggle.addEventListener('click', toggleDarkMode);
 function toggleDarkMode() {
   body.classList.toggle('dark-mode');
 }
+
+const form = document.querySelector('form');
+const passwordInput = document.getElementById('inputSenha');
+const confirmPasswordInput = document.getElementById('inputConfirmarSenha');
+const emailInput = document.getElementById('inputEmail');
+
+form.addEventListener('submit', function(event) {
+  if (!validatePassword()) {
+    event.preventDefault();
+  }
+
+  if (!validateEmail()) {
+    event.preventDefault();
+  }
+});
+
+function validatePassword() {
+  const passwordValue = passwordInput.value;
+  const confirmPasswordValue = confirmPasswordInput.value;
+  
+  if (passwordValue !== confirmPasswordValue) {
+    confirmPasswordInput.setCustomValidity('As senhas não são iguais.');
+    return false;
+  } else {
+    confirmPasswordInput.setCustomValidity('');
+    return true;
+  }
+}
+
+function validateEmail() {
+  const emailValue = emailInput.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(emailValue)) {
+    emailInput.setCustomValidity('Digite um e-mail válido.');
+    return false;
+  } else {
+    emailInput.setCustomValidity('');
+    return true;
+  }
+}
